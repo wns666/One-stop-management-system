2b28fe4fc907878f35120d6d4b93420509b9d6d3
wns666 <48899682+wns666@users.noreply.github.com>
2022-03-14 21:47:03 +0800

    登录和注册功能完成

--- system/djangoA/polls/urls.py
+++ system/djangoA/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 #app_name='polls'
 urlpatterns = [
     path('', views.toLogin_view),  # 与views里面的相应
-    # path('index/', views.Login_view),
-    # path('toregister/', views.toregister_view),
-    # path('register/', views.register_view),
+    path('index/', views.Login_view),
+    path('toregister/', views.toregister_view),
+    path('register/', views.register_view),
 ]
--- system/djangoA/polls/views.py
+++ system/djangoA/polls/views.py
@@ -9,33 +9,41 @@ from django.shortcuts import render
 from .models import *
 
 
+#
 def toLogin_view(request):
     return render(request, '../templates/login.html', {'name': 'wlqtc'})
 
 
-# def toregister_view(request):
-#     return render(request, 'register.html')
+#
+def toregister_view(request):
+    return render(request, '../template/register.html')
 
 
-# def register_view(request):
-#     u = request.POST.get("user", '')
-#     p = request.POST.get("pwd", '')
-#     if u and p:
-#         stu = PollsStudentinfo(stu_name=u, stu_pwd=p)
-#         stu.save()
-#         return HttpResponse("注册成功")
-#     else:
-#         return HttpResponse("注册失败")
-#
-#
-# def Login_view(request):
-#     u = request.POST.get("user", '')
-#     p = request.POST.get("pwd", '')
-#     if u and p:
-#         c = PollsStudentinfo.objects.filter(stu_name=u, stu_pwd=p).count()
-#         if c >= 1:
-#             return HttpResponse("登录成功")  # 给个响应
-#         else:
-#             return HttpResponse("账号密码错误")  # 给个响应
-#     else:
-#         return HttpResponse("账号密码错误")
+def toregister_view(request):
+    return render(request, 'register.html')
+
+
+def register_view(request):
+    u = request.POST.get("w_no", '')
+    n = request.POST.get("w_name", '')
+    p = request.POST.get("w_pass", '')
+    c = WInit.objects.filter(w_no=u, w_name=n).count()
+    if u and n and p and c:
+        zc = WStup(w_ano=u, w_pass=p)
+        zc.save()
+        return HttpResponse("注册成功")
+    else:
+        return HttpResponse("注册失败")
+
+
+def Login_view(request):
+    u = request.POST.get("w_ano", '')
+    p = request.POST.get("w_pass", '')
+    if u and p:
+        c = WStup.objects.filter(w_ano=u, w_pass=p).count()
+        if c >= 1:
+            return HttpResponse("登录成功")  # 给个响应
+        else:
+            return HttpResponse("账号密码错误")  # 给个响应
+    else:
+        return HttpResponse("账号密码错误")
